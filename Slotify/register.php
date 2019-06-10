87f87be49d3084f93bd15b4b7361fdf86729476b
Michal_Piec <m.buczek@mat.net.pl>
2019-06-10 23:09:40 +0200

    Styling of register/login page. Remembereing input data after page reload with php getInputValue()

--- Slotify/register.php
+++ Slotify/register.php
@@ -27,6 +27,27 @@
     <script src="assets/js/register.js"></script>
 </head>
 <body>
+
+    <?php 
+    if(isset($_POST['registerButton'])) {
+		echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").hide();
+					$("#registerForm").show();
+				});
+			</script>';
+	}
+	else {
+		echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").show();
+					$("#registerForm").hide();
+				});
+			</script>';
+	}
+    ?>
+    
+
     <div id="background">
         <div id="loginContainer">
 
@@ -36,7 +57,7 @@
                     <p>
                         <?php echo $account->getError(Constants::$loginFailed); ?>
                         <label for="loginUsername">Username</label>
-                        <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. Michal Buczek" required>   
+                        <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. Michal Buczek" value="<?php getInputValue('loginUsername') ?>" required>   
                     </p>
                     <p>
                         
@@ -94,6 +115,16 @@
                         <span id="hideRegister">Already have an account? Log in here</span>
                     </div>
                 </form>
+            </div>
+            <div id="loginText">
+                <h1>Get great music, right now<h1>
+                <h2>Listen to loads of songs right now<h2>
+                <ul>
+                    <li>Discover music you'll fall in love with</li>
+                    <li>Create Your own playlist</li>
+                    <li>Follow artists to keep up to date</li>
+                </ul>
+
             </div>
         </div>
     </div>
